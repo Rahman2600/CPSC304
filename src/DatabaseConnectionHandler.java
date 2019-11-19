a876dc8c28ff4125d8e9a9a7a0887126c8e45cb7
rahman2600 <abdurrahmanmudasiru@yahoo.com>
2019-11-19 14:26:59 -0800

    Started on Rent

--- src/DatabaseConnectionHandler.java
+++ src/DatabaseConnectionHandler.java
@@ -1,4 +1,6 @@
 import model.ReceiptModel;
+import model.RentModel;
+import model.ReservationModel;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -28,8 +30,60 @@ public class DatabaseConnectionHandler {
         }
     }
 
-    public ReceiptModel rentVehicle() {
+    public ReceiptModel rentVehicle(RentModel model) {
+        //TODO: figure out types
+        try {
+            PreparedStatement ps = connection.prepareStatement("INSERT INTO rent VALUES (?,?,?,?,?,?,?,?,?,?,?,?)");
+            ps.setInt(1, model.getRid());
+            ps.setInt(2, model.getVid());
+            ps.setString(3, model.getCellNum());
+            ps.setString(4, model.getFromDate());
+            ps.setString(5, model.getToDate());
+            ps.setString(6, model.getFromTime());
+            ps.setString(7, model.getToDate());
+            ps.setString(8, model.getToTime());
+            ps.setString(9, model.getOdometer());
+            ps.setString(10, model.getCardName());
+            ps.setString(11, model.getExpDate());
+            ps.setString(12, model.getConfNo());
+            connection.commit();
 
+            ps.close();
+        } catch (SQLException e) {
+            System.out.println(EXCEPTION_TAG + " " + e.getMessage());
+            rollbackConnection();
+        }
+    }
+
+    public ReservationModel getReservation(int confNo) {
+        try {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT FROM reservation WHERE confNo = ?");
+            if (rs.next()) {
+                return new ReservationModel(   rs.getInt("confNo"),
+                                                rs.getString("vtname"),
+                                                rs.getString("cellphone"),
+                                                rs.getString("fromDate"),
+                                                rs.getString("fromTime"),
+                                                rs.getString("toDate"),
+                                                rs.getString("toTime"));
+            }
+        } catch (SQLException e) {
+            System.out.println(EXCEPTION_TAG + " " + e.getMessage());
+        }
+        return null;
+    }
+
+    public ReservationModel getReservation(String cellNum) {
+
+    }
+
+    private void rollbackConnection() {
+        try  {
+            connection.rollback();
+        } catch (SQLException e) {
+            System.out.println(EXCEPTION_TAG + " " + e.getMessage());
+        }
     }
 
 }
--- src/model/ReservationModel.java
+++ src/model/ReservationModel.java
@@ -1,4 +1,49 @@
 package model;
 
 public class ReservationModel {
+    private final int confNo;
+    private final String vtname;
+    private final String cellphone;
+    private final String fromDate;
+    private final String fromTime;
+    private final String toDate;
+    private final String toTime;
+
+    public ReservationModel(int confNo, String vtname, String cellphone, String fromDate, String fromTime, String toDate, String toTime) {
+        this.confNo = confNo;
+        this.vtname = vtname;
+        this.cellphone = cellphone;
+        this.fromDate = fromDate;
+        this.fromTime = fromTime;
+        this.toDate = toDate;
+        this.toTime = toTime;
+    }
+
+    public int getConfNo() {
+        return confNo;
+    }
+
+    public String getVtname() {
+        return vtname;
+    }
+
+    public String getCellphone() {
+        return cellphone;
+    }
+
+    public String getFromDate() {
+        return fromDate;
+    }
+
+    public String getFromTime() {
+        return fromTime;
+    }
+
+    public String getToDate() {
+        return toDate;
+    }
+
+    public String getToTime() {
+        return toTime;
+    }
 }
